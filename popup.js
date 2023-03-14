855ebf2cc8a96dcb322cfec1382ecc4565c8f6d3
Azarafath <ahmadzakariafathoni@gmail.com>
2023-03-15 05:25:30 +0700

    Push to github

---
+++ popup.js
@@ -0,0 +1,26 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    var checkButton = document.getElementById("translateBtn");
+    checkButton.addEventListener(
+      "click",
+      function () {
+        chrome.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs) {
+            chrome.tabs.sendMessage(
+              tabs[0].id,
+              { method: "translate" },
+              function (response) {
+                if (response.method == "translate") {
+                }
+              }
+            );
+          }
+        );
+      },
+      false
+    );
+  },
+  false
+);
